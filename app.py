988430a40061cdb82b63cc3ac78767be6ff8112b
mikhail <sora@synzr.ru>
2023-03-01 13:58:06 +0500

    Basic upload

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort
+from nanoid import generate as nanoid
+import mimetypes
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(256)
+
+database = {}
 
 
 @app.get("/")
 def index():
     return render_template("index.html")
+
+
+@app.put("/files")
+def upload_file():
+    if not request.files or "file" not in request.files:
+        return abort(400)
+
+    file = request.files["file"]
+
+    file_id = nanoid()
+    extension = mimetypes.guess_extension(file.content_type)
+
+    database[file_id] = f"/static/user_uploads/{file_id}{extension}"
+    file.save(database[file_id][1:])
+
+    return f"File was uploaded. {database[file_id]}"
---
+++ static/js/drag-and-drop.js
@@ -0,0 +1,38 @@
+const uploadForm = document.getElementById("upload-form")
+const dragAndDropInput = document.getElementById("file")
+const dragAndDropElement = document.getElementById("drag-and-drop")
+const uploadProgress = document.getElementById("upload-progress")
+
+const preventDefaults = (element) => {
+  element.preventDefault()
+  element.stopPropagation()
+}
+
+const handleDrop = (element) => {
+  const { files } = element.dataTransfer
+
+  if (files.length !== 1) {
+    return
+  }
+
+  handleFiles(files)
+}
+
+const handleFiles = (files) => {
+  dragAndDropInput.files = files
+
+  htmx.on('#upload-form', 'htmx:xhr:progress',
+    (event) => htmx
+      .find('#upload-progress')
+      .setAttribute('value', event.detail.loaded / event.detail.total * 100)
+  )
+
+  uploadForm.requestSubmit()
+  uploadProgress.classList.remove("invisible")
+}
+
+['dragenter', 'dragover', 'dragleave', 'drop'].forEach(
+  (event) => dragAndDropElement.addEventListener(event, preventDefaults, false)
+)
+
+dragAndDropElement.addEventListener('drop', handleDrop, false)
